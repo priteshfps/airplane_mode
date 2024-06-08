387e5e152cf8a18fddbb0b89716ed93b79f67e43
Pritesh Patel <Pritesh@5pointsol.com>
2024-06-08 08:52:09 +0000

    changes

--- airplane_mode/airport_shop_management/doctype/payment_receipt/payment_receipt.py
+++ airplane_mode/airport_shop_management/doctype/payment_receipt/payment_receipt.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024, air and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 from frappe.utils import money_in_words
 
@@ -11,8 +11,8 @@ class PaymentReceipt(Document):
 		#frappe.throw("Tes")
 		#self.full_name = ' '.join(filter(lambda x: x, [self.first_name,   self.last_name]))
 		self.amount_in_words = money_in_words(self.amount, main_currency=None, fraction_currency=None)
-		doc = frappe.get_doc("Contract Invoice", self.contract_invoice)
-		doc.payment_date = self.payment_receipt_date
-		doc.save() 
+		#docci = frappe.get_doc("Contract Invoice", self.contract_invoice)
+		#docci.payment_date = self.payment_receipt_date
+		#docci.save() 
 
 
--- airplane_mode/airport_shop_management/doctype/shop_contract/shop_contract.py
+++ airplane_mode/airport_shop_management/doctype/shop_contract/shop_contract.py
@@ -29,7 +29,7 @@ class ShopContract(Document):
 			days = invoice_startdate.day
 			#frappe.msgprint(str(invoice_startdate.day))
 			invdate =   add_to_date(invoice_startdate, days=days*-1,months=1, as_datetime=True)
-			frappe.msgprint(str(self.name))
+			#frappe.msgprint(str(self.name))
 			while invdate <=  invoice_Enddate:
 				nameseries = "CI-"+self.shop+invdate.strftime('-%y%m-')+""
 				#frappe.msgprint( nameseries)
@@ -62,36 +62,4 @@ class ShopContract(Document):
 		return date.replace(month=date.month+1, day=1) - timedelta(days=1)
 
 	#yesterday = add_days(today(), -1)
-	send_rent_reminder = frappe.db.get_single_value("Global Configuration", "send_rent_reminder")
-	if (send_rent_reminder):
-		data= frappe.db.sql(
-			"""select t.full_name ,t.email,s.name,ci.name ciname,ci.contract, ci.contract_invoice_date,ci.amount,LAST_DAY(CURRENT_DATE)  from `tabContract Invoice` ci inner join
-			`tabAirport Shop` s on s.name = ci.shop inner join `tabTenant` t on t.name = ci.tenant  left join `tabPayment Receipt` pr on ci.name = pr.contract_invoice where ci.contract_invoice_date <=
-			LAST_DAY(CURRENT_DATE) and pr.amount is null ; 
-			""",
-			as_dict=1,
-		)
-
-		for d in data:
-			
-			message = (
-			_("Hello {0},").format(d.full_name)
-			+ "<br><br>"
-			+ _("Reminder for shop contract payment due of contract No. : {0}").format(
-				d.contract
-			)
-			+ "."
-			+ "<br><br>"
-			+ _(" you have pending amount : {0} ,  against contract invoice# : {1} for the shop : {2}. invoice date is {3}").format(
-				d.amount,d.ciname,d.name,d.contract_invoice_date
-			)
-			+ "."
-			+ "<br><br>"
-			+ _("Please pay earliest to avoid penalty charges ")
-			)
-			frappe.sendmail(
-				recipients=d.email,
-				subject=_("Reminder for Rent Due of contract - {0}").format(d.contract),
-				message=message,
-			)
-
+	
